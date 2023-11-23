78786fb75b59aebabf13412f9b7d9be6579ff757
Vivek Sunil Jadhav <vivek.jadhav.edu@gmail.com>
2023-11-23 21:37:05 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Navbar from "./Components/Navbar"
+
 function App() {
 
   return (
     <>
+      <Navbar />
       <h1 className="text-3xl font-bold flex items-center h-screen justify-center ">
         Hello Mrs. Sahyadri Aditya Patil...😁😁😁
       </h1>
+
     </>
   )
 }
